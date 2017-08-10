a79b64bc302450647028ec74ae410c05b70f54c5
NuthanReddy <nuthan.reddy999@gmail.com>
2017-08-10 23:47:21 +0530

    working tabs

--- mlearn/urls.py
+++ mlearn/urls.py
@@ -10,6 +10,9 @@ urlpatterns = [
     url(r'^logout_user/$', views.logout_user, name='logout_user'),
     #url(r'^(?P<competition_id>[0-9]+)/$', views.detail, name='detail'),
     url(r'^(?P<competition_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<competition_id>[0-9]+)/data$', views.data, name='data'),
+    url(r'^(?P<competition_id>[0-9]+)/score$', views.score, name='score'),
+    url(r'^(?P<competition_id>[0-9]+)/submit$', views.submit, name='submit'),
     url(r'^add_dataset/$', views.create_dataset, name='create_dataset'),
     url(r'^(?P<dataset_id>[0-9]+)/delete_dataset/$', views.delete_dataset, name='delete_dataset'),
 ]

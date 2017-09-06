0c388fa2051fa64e410ec5c7fff5e0b9181aa9b4
nuthan <nuthan.reddy999@gmail.com>
2017-09-06 19:43:23 +0530

    working rmse logic

--- comp/views.py
+++ comp/views.py
@@ -1,19 +1,22 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
-
 from comp.forms import SubmitForm
 from comp.models import Registration, Competition, Submission
-
-# from pyspark.sql import SparkSession
+from pandas import read_table
+from sklearn.metrics import mean_squared_error
 
 ACCEPTED_FILE_TYPES = ['csv']
 
 
+# from pyspark.sql import SparkSession
 # spark = SparkSession \
 #    .builder \
 #    .appName("Data Crunch") \
 #    .config("spark.some.config.option", "some-value") \
 #    .getOrCreate()
+
+# spark = SparkSession.builder.master("local").appName("Data Crunch").\
+#    config("spark.sql.warehouse.dir", "hdfs://localhost:8020/user/spark/warehouse").getOrCreate()
 
 # df = spark.read.csv.options(header='true', inferSchema='true').load("examples/src/main/resources/people.json")
 
@@ -52,6 +55,7 @@ def register(request, competition_id):
 
 @login_required
 def submit(request, competition_id):
+    global scr
     competition = get_object_or_404(Competition, pk=competition_id)
     if request.method == 'GET':
         form = SubmitForm()
@@ -64,19 +68,35 @@ def submit(request, competition_id):
             file_type = newsub.file_submission.url.split('.')[-1].lower()
             if file_type not in ACCEPTED_FILE_TYPES:
                 args = {
-                    'form': form,
-                    'competition': competition,
-                    'error_message': 'File must be in CSV format',
-                }
+                    'form': form, 'competition': competition,
+                    'error_message': 'File must be in CSV format'}
                 return render(request, 'comp/submit.html', args)
             newsub.file_submission = request.FILES['file_submission']
             newsub.user = request.user
             newsub.comp = competition
             newsub.language = form.cleaned_data['language']
             newsub.algo = form.cleaned_data['algo']
-            newsub.score = 0.5
+            # score calculation
+            header = 0
+            sep = ','
+            index_col = None
+            scoring_formula = "RMSE"
+            predict_path = newsub.file_submission
+            p_table = read_table(predict_path, header=header, index_col=index_col, sep=sep, skiprows=0,
+                                 nrows=None, skip_blank_lines=True)
+            pcols = p_table.columns
+            if scoring_formula == "RMSE":
+                valid_path = competition.valid
+                v_table = read_table(valid_path, header=header, index_col=index_col, sep=sep, skiprows=0,
+                                     nrows=None, skip_blank_lines=True)
+                vcols = v_table.columns
+                if pcols.all() == vcols.all():
+                    newsub.score = round(1 / mean_squared_error(v_table, p_table, multioutput='raw_values')[-1], 2)
+                else:
+                    args = {'form': form, 'competition': competition,
+                            'error_message': 'Columns does not match in uploaded submission'}
+                    return render(request, 'comp/submit.html', args)
             newsub.save()
-            # need to change
             subs = Submission.objects.filter(comp=newsub.comp).order_by('score').reverse()
             return render(request, 'comp/score.html', {'competition': competition, 'submissions': subs})
         else:
